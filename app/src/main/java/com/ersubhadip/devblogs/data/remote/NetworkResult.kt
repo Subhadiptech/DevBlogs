a70da794ec843e015c902fb64fd97e6859e6ab21
Subhadiptech <subhadipdhn@gmail.com>
2023-01-28 22:32:52 +0530

    networking config

--- app/src/main/java/com/ersubhadip/devblogs/data/remote/NetworkResult.kt
+++ app/src/main/java/com/ersubhadip/devblogs/data/remote/NetworkResult.kt
@@ -1,5 +1,7 @@
 package com.ersubhadip.devblogs.data.remote
 
-sealed class NetworkResult {
-
+sealed class NetworkResult<T:Any> {
+  class Success<T: Any>(val data: T) : NetworkResult<T>()
+  class Error<T: Any>(val code: Int, val message: String?) : NetworkResult<T>()
+  class Exception<T: Any>(val e: Throwable) : NetworkResult<T>()
 }
